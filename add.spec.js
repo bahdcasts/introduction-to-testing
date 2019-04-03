18e9e5ff46a0bbbdf562b5224bdfb549050ee490
Kati Frantz <bahdcoder@gmail.com>
2019-04-03 22:43:41 +0100

    lesson-2-what-is-an-assertion

--- add.spec.js
+++ add.spec.js
@@ -1,9 +1,7 @@
 const add = require('./add')
+const assert = require('assert')
 
 const result = add(1, 3)
+const expected = 4
 
-if (result == 4) {
-    console.log('tests pass.')
-} else {
-    throw new Error('Expected 1 + 3 to equal 4')
-}
+assert.equal(result, expected, 'Add function does not work properly.')
